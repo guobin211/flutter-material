0df5bad12d5f2d8a9c375e392e3fbc9be2dfee91
GuoBin <guobin201314@gmail.com>
2019-08-17 11:07:41 +0800

    flutter demo app

---
+++ lib/models/post.dart
@@ -0,0 +1,6 @@
+class Post {
+  final String author;
+  final String title;
+  final String imgUrl;
+  const Post({this.title, this.author, this.imgUrl});
+}
---
+++ lib/widgets/drawer_demo.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+/// 抽屉组件
+class DrawerDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text('header'),
+            decoration: BoxDecoration(color: Colors.grey[100]),
+          ),
+          ListTile(
+            title: Text('Message', textAlign: TextAlign.right),
+            trailing: Icon(Icons.message, color: Colors.black12, size: 22.0),
+            onTap: () => Navigator.pop(context),
+          ),
+          ListTile(
+            title: Text('Favorite', textAlign: TextAlign.right),
+            trailing: Icon(Icons.favorite, color: Colors.black12, size: 22.0),
+            onTap: () => Navigator.pop(context),
+          ),
+          ListTile(
+            title: Text('Settings', textAlign: TextAlign.right),
+            trailing: Icon(Icons.settings, color: Colors.black12, size: 22.0),
+            onTap: () => Navigator.pop(context),
+          ),
+        ],
+      ),
+    );
+  }
+}
