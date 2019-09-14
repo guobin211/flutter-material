714785eee6062c8b29a22552cad11610aba5dcc4
GuoBin <guobin201314@gmail.com>
2019-09-14 10:56:17 +0800

    list

--- lib/widgets/list_view_demo.dart
+++ lib/widgets/list_view_demo.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
+import '../models/post.dart';
 
-class ListViewDemo extends StatelessWidget{
+class ListViewDemo extends StatelessWidget {
+  final double _fontSize = 32.0;
+  /// item weiget
+  Widget _pageItemBuilder(BuildContext context, int index) {
+    return Stack(
+      children: <Widget>[
+        SizedBox.expand(
+          child: Image.network(
+            posts[index].imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          bottom: 8.0,
+          left: 8.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                posts[index].author,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                posts[index].title,
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        ListTile(
-          title: Text("标题"),
-          subtitle: Text("详细信息"),
-        ),
-        ListTile(
-          title: Text("标题"),
-          subtitle: Text("详细信息"),
-        )
-      ],   
-    );
+    return PageView.builder(
+
+        /// 垂直滚动
+        scrollDirection: Axis.vertical,
+        itemCount: posts.length,
+        itemBuilder: _pageItemBuilder);
   }
 }
