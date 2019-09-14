e271f2d0374d34cc67941a11e1db6bbf61b51f36
GuoBin <guobin201314@gmail.com>
2019-09-14 10:55:58 +0800

    抽屉

--- lib/widgets/drawer_demo.dart
+++ lib/widgets/drawer_demo.dart
@@ -8,9 +8,28 @@ class DrawerDemo extends StatelessWidget {
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
-          DrawerHeader(
-            child: Text('header'),
-            decoration: BoxDecoration(color: Colors.grey[100]),
+//          DrawerHeader(
+//            child: Text('header'),
+//            decoration: BoxDecoration(color: Colors.grey[100]),
+//          ),
+          UserAccountsDrawerHeader(
+            accountName:
+            Text('guobin'.toUpperCase(), style: TextStyle(fontWeight: FontWeight.bold)),
+            accountEmail: Text('guobin201314@gmail.com'),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://avatars1.githubusercontent.com/u/30451385?s=460&v=4'),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.yellow[400],
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://resources.ninghao.org/images/childhood-in-a-picture.jpg'),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                    Colors.yellow[400].withOpacity(0.6), BlendMode.hardLight),
+              ),
+            ),
           ),
           ListTile(
             title: Text('Message', textAlign: TextAlign.right),
