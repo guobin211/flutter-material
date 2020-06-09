aa7d2e825eff2f837a53c5b54522ca9691ee28ed
GuoBin <guobin201314@gmail.com>
2020-06-09 21:27:22 +0800

    state-manage

--- chat_ui/lib/models/user_model.dart
+++
@@ -1,9 +0,0 @@
-class User {
-
-  final String name;
-
-  final int id;
-
-  final String imageUrl;
-  User({this.id, this.name, this.imageUrl});
-}
--- sidebar_animation/lib/constants/app_color.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColor {
-
-  static const primary = Color(0xFF262AAA);
-
-  static const warning = Colors.amber;
-
-}
