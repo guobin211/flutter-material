e90fa0fcc710d1ba38ec4017f9fee637bcd47a24
GuoBin <guobin201314@gmail.com>
2020-06-10 10:58:34 +0800

    feature(app): list
    listview使用

---
+++ lib/components/post_list_view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:fluttermaterial/mock/post.dart';
+
+class PostListView extends StatelessWidget {
+  Widget _listItemBuilder(BuildContext context, int index) {
+    final item = posts[index];
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Image.network(item.imageUrl),
+          SizedBox(
+            height: 16.0,
+          ),
+          Text(
+            item.title,
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          Text(
+            item.author,
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+        ],
+      ),
+      color: Colors.white,
+      margin: EdgeInsets.all(8.0),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: posts.length, itemBuilder: _listItemBuilder);
+  }
+}
