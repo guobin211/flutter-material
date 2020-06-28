cd1bb732b233e35c5fb4f1ab1de95eda4a4de73e
GuoBin <guobin201314@gmail.com>
2020-06-28 11:41:08 +0800

    animation(flutter): animate mixin

--- lib/animations/app_animate.dart
+++ lib/animations/app_animate.dart
@@ -11,9 +11,75 @@ class AppAnimate extends StatefulWidget {
   _AppAnimateState createState() => _AppAnimateState();
 }
 
-class _AppAnimateState extends State<AppAnimate> {
+class _AppAnimateState extends State<AppAnimate>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation<double> _animation;
+  Animation _animationColor;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+        duration: const Duration(milliseconds: 500), vsync: this);
+    _animation =
+        Tween<double>(begin: 32.0, end: 100.0).animate(_animationController)
+          ..addListener(() {
+            setState(() {});
+          })
+          ..addStatusListener((status) {
+            print("_animation status: $status");
+          });
+
+    _animationColor = ColorTween(begin: Colors.redAccent, end: Colors.red[900])
+        .animate(_animationController)
+          ..addListener(() {
+            setState(() {});
+          })
+          ..addStatusListener((status) {
+            print("_animationColor status: $status");
+          });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  _pressed() {
+    print("_pressed");
+    switch (_animationController.status) {
+      case AnimationStatus.completed:
+        {
+          _animationController.reverse();
+        }
+        break;
+      case AnimationStatus.dismissed:
+        {
+          _animationController.forward();
+        }
+        break;
+      default:
+        {
+          return;
+        }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: GestureDetector(
+          onTap: _pressed,
+          child: Icon(
+            Icons.favorite,
+            size: _animation.value,
+            color: _animationColor.value,
+          ),
+        ),
+      ),
+    );
   }
 }
